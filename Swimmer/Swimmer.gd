72b8408eaf4ed3959c468c456fcbac1e77a2a97e
Lukas Prost <e1225511@student.tuwien.ac.at>
2018-08-12 11:46:16 +0200

    spawn players into pool

--- Swimmer/Swimmer.gd
+++ Swimmer/Swimmer.gd
@@ -1,63 +1,80 @@
 extends RigidBody2D
 
 enum State {
-	SPAWNED,
 	GOTO_WATER,
 	IN_WATER,
 	LEAVE_WATER
 }
 
-var force_arrow
+export var speed = 5.0
+export var initial_force_magnitude = 30.0
 
-var state = State.SPAWNED
+var force_arrow
+var state
+
 var selected = false
-var walk_direction = Vector2(0, 0)
+var walk_target = Vector2()
+var initial_force = Vector2()
 
 func _ready():
 	$Area2D.connect("input_event", self, "_on_input_event")
+	$Area2D.connect("area_entered", self, "_on_area_entered")
 	
 	force_arrow = $ForceArrow
 	force_arrow.visible = false
 	
-	set_collision_mask_bit(Constants.POOL_WAIT_LAYER, true)
-	set_collision_mask_bit(Constants.POOL_BORDER_LAYER, true)
-	set_collision_mask_bit(Constants.SWIMMER_LAYER, true)
-	
 	set_process(true)
 
-func entered_water():
-	set_collision_mask_bit(Constants.POOL_WAIT_LAYER, false)
-	set_collision_mask_bit(Constants.POOL_BORDER_LAYER, true)
-	set_collision_mask_bit(Constants.SWIMMER_LAYER, true)
-	
-	state = State.IN_WATER
+func goto_water():
+	_set_state(State.GOTO_WATER)
 
 func leave_water():
-	set_collision_mask_bit(Constants.POOL_WAIT_LAYER, false)
-	set_collision_mask_bit(Constants.POOL_BORDER_LAYER, false)
-	set_collision_mask_bit(Constants.SWIMMER_LAYER, false)
-	
-	state = State.LEAVE_WATER
+	_set_state(State.LEAVE_WATER)
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		force_arrow.visible = true
 		selected = true
 
-func _process(delta):
+func _on_area_entered(area):
+	if(area.get_collision_layer_bit(Constants.ENTER_WATER_LAYER)):
+		$Move.stop_all()
+		apply_impulse(Vector2(), initial_force)
 	
+	if(area.get_collision_layer_bit(Constants.WATER_AREA_LAYER)):
+		_set_state(State.IN_WATER)
+
+func _set_state(state):
 	match state:
 		State.GOTO_WATER:
-			print("")
-		State.SPAWNED:
-			print("")
+			$Area2D.set_collision_mask_bit(Constants.ENTER_WATER_LAYER, true)
+			$Area2D.set_collision_mask_bit(Constants.WATER_AREA_LAYER, true)
+			
+			self.set_collision_mask_bit(Constants.POOL_BORDER_LAYER, false)
+			self.set_collision_mask_bit(Constants.SWIMMER_LAYER, false)
+			
+			initial_force = (walk_target - self.position).normalized() * initial_force_magnitude
+			
+			$Move.interpolate_property(self, "position", self.position, walk_target, 3.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+			$Move.start()
+			
 		State.IN_WATER:
-			print("")
-		State.LEAVE:
-			print("")
+			$Area2D.set_collision_mask_bit(Constants.ENTER_WATER_LAYER, false)
+			$Area2D.set_collision_mask_bit(Constants.WATER_AREA_LAYER, false)
+			
+			self.set_collision_mask_bit(Constants.POOL_BORDER_LAYER, true)
+			self.set_collision_mask_bit(Constants.SWIMMER_LAYER, true)
+			
+		State.LEAVE_WATER:
+			$Area2D.set_collision_mask_bit(Constants.ENTER_WATER_LAYER, false)
+			$Area2D.set_collision_mask_bit(Constants.WATER_AREA_LAYER, false)
+			
+			self.set_collision_mask_bit(Constants.POOL_BORDER_LAYER, false)
+			self.set_collision_mask_bit(Constants.SWIMMER_LAYER, false)
 
-
-	if selected:
+func _process(delta):
+	
+	if state == State.IN_WATER and selected:
 		if Input.is_action_pressed("select"):
 			force_arrow.set_end_pos(get_viewport().get_mouse_position() - self.position)
 		else:
