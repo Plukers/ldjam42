5352893ec83919678e1379a11aff5cf979d91bb0
Johannes Erns Pauschenwein <e1427350@student.tuwien.ac.at>
2018-08-13 22:53:30 +0200

    add sound

--- Swimmer/Swimmer.gd
+++ Swimmer/Swimmer.gd
@@ -14,9 +14,35 @@ enum Status {
 	REALLY_WANTS_OUT,
 	HE_DEAD
 	}
+enum Sound {
+	DIE,
+	COLLIDE,
+	JUMP,
+	SAVE,
+	CLICK_DOWN,
+	CLICK_RELEASE,
+	HELP,
+	HELP_HELP
+	}
 
 var skull = preload("res://Swimmer/Skull.tscn")
 var happy = preload("res://Swimmer/Happy.tscn")
+
+var collide_0 = preload("res://Assets/Swimmer/collide_0.wav")
+var collide_1 = preload("res://Assets/Swimmer/collide_1.wav")
+var save_0 = preload("res://Assets/Swimmer/save_0.wav")
+var die_0 = preload("res://Assets/Swimmer/die_0.wav")
+var die_1 = preload("res://Assets/Swimmer/die_1.wav")
+var die_2 = preload("res://Assets/Swimmer/die_2.wav")
+var jump_0 = preload("res://Assets/Swimmer/jump_0.wav")
+var jump_1 = preload("res://Assets/Swimmer/jump_1.wav")
+var jump_2 = preload("res://Assets/Swimmer/jump_2.wav")
+var help_0 = preload("res://Assets/Swimmer/help_0.wav")
+var help_1 = preload("res://Assets/Swimmer/help_1.wav")
+var help_help_0 = preload("res://Assets/Swimmer/help_help_0.wav")
+var help_help_1 = preload("res://Assets/Swimmer/help_help_1.wav")
+var click_down = preload("res://Assets/Swimmer/click_down_0.wav")
+var click_release = preload("res://Assets/Swimmer/click_release_0.wav")
 
 export var speed = 5.0
 export var initial_force_magnitude = 40.0
@@ -33,8 +59,9 @@ var status = Status.HAPPY
 func _ready():
 	$Area2D.connect("input_event", self, "_on_input_event")
 	$Area2D.connect("area_entered", self, "_on_area_entered")
-	$Move.connect("tween_completed", self, "_on_tween_ended");
+	$Move.connect("tween_completed", self, "_on_tween_ended")
 	$Timer.connect("timeout", self, "_on_time_out")
+	self.connect("body_entered", self, "_on_collision_swimmer")
 
 	force_arrow = $ForceArrow
 	force_arrow.visible = false
@@ -54,13 +81,63 @@ func leave_water():
 	happy_instance.translate(Vector2(0, -40))
 	self.add_child(happy_instance)
 	$Timer.stop()
+	_play_sound(Sound.SAVE)
 	
 
+func _play_sound(sound):
+	match sound:
+		Sound.COLLIDE:
+			match randi()%1:
+				0:
+					$Audio.stream = collide_0
+				1:
+					$Audio.stream = collide_1
+		Sound.DIE:
+			match randi()%2:
+				0:
+					$Audio.stream = die_0
+				1:
+					$Audio.stream = die_1
+				2:
+					$Audio.stream = die_2
+		Sound.SAVE:
+			$Audio.stream = save_0
+		Sound.JUMP:
+			match randi()%2:
+				0:
+					$Audio.stream = jump_0
+				1:
+					$Audio.stream = jump_1
+				2:
+					$Audio.stream = jump_2
+		Sound.HELP:
+			match randi()%1:
+				0:
+					$Audio.stream = help_0
+				1:
+					$Audio.stream = help_1
+		Sound.HELP_HELP:
+			match randi()%1:
+				0:
+					$Audio.stream = help_help_0
+				1:
+					$Audio.stream = help_help_1
+		Sound.CLICK_DOWN:
+			$Audio.stream = click_down
+		Sound.CLICK_RELEASE:
+			$Audio.stream = click_release
+	$Audio.play()
 
 func _on_input_event(viewport, event, shape_idx):
 	if state == State.IN_WATER and status != Status.HE_DEAD and event is InputEventMouseButton and event.pressed:
 		force_arrow.visible = true
 		selected = true
+		_play_sound(Sound.CLICK_DOWN)
+
+func _on_collision_swimmer(body):
+	print("something collided!!")
+	if(body.get_type() == "RigidBody2D"):
+		_play_sound(Sound.COLLIDE)
 
 func _on_area_entered(area):
 	if(area.get_collision_layer_bit(Constants.WATER_AREA_LAYER)):
@@ -68,9 +145,9 @@ func _on_area_entered(area):
 		apply_impulse(Vector2(), initial_force)
 		_set_state(State.IN_WATER)
 		$AnimatedSprite.set_animation("swim")
+		_play_sound(Sound.JUMP)
 
 func _on_tween_ended(object, key):
-	print(self, "is freed")
 	self.queue_free()
 
 func _on_time_out():
@@ -78,11 +155,13 @@ func _on_time_out():
 		$AnimatedSprite.set_animation("help_1")
 		status += 1
 		self.set_collision_layer_bit(Constants.SWIMMER_WANTS_OUT, true)
+		_play_sound(Sound.HELP)
 		
 		$Timer.wait_time = randi() % 7 + 2
 		$Timer.start()
 	elif(status == Status.WANTS_OUT):
 		$AnimatedSprite.set_animation("help_2")
+		_play_sound(Sound.HELP_HELP)
 		status += 1
 		
 		$Timer.wait_time = randi() % 7 + 2
@@ -94,6 +173,7 @@ func _on_time_out():
 		var skull_instance = skull.instance()
 		skull_instance.translate(Vector2(0, -40))
 		self.add_child(skull_instance)
+		_play_sound(Sound.DIE)
 		emit_signal("died")
 
 func _set_state(new_state):
@@ -143,4 +223,5 @@ func _process(delta):
 			force_arrow.visible = false
 			selected = false
 			apply_impulse(Vector2(0, 0),  force_arrow.get_force_vector())
+			_play_sound(Sound.CLICK_RELEASE)
 
