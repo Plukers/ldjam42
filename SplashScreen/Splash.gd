35408e8262775051f982c0dd8821a3342b5f6802
joschi1212 <e1427350@student.tuwien.ac.at>
2019-08-24 15:07:37 +0200

    add how to play button

--- SplashScreen/Splash.gd
+++ SplashScreen/Splash.gd
@@ -3,6 +3,7 @@ extends Node2D
 func _ready():
 	$Animate.interpolate_property($David, "position", $David.position, Vector2(448, 576), 1.0, Tween.TRANS_SINE, Tween.EASE_OUT)
 	$Animate.interpolate_property($PlayButton, "position", $PlayButton.position, Vector2(976, 576), 1.0, Tween.TRANS_SINE, Tween.EASE_OUT)
+	$Animate.interpolate_property($HowToButton, "position", $HowToButton.position, Vector2(976, 440), 1.0, Tween.TRANS_SINE, Tween.EASE_OUT)
 	$Animate.interpolate_property($Title, "rect_position", $Title.rect_position, Vector2(144, 0), 1.0, Tween.TRANS_SINE, Tween.EASE_OUT)
 	
 	
